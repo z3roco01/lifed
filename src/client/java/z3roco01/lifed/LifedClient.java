671d1f1492ffc4666d8f0acdfb4690bf931714a6
z3roco01 <zerocool@e.email>
2025-12-13 17:09:34 -0600

    remove & change defaults

--- src/client/java/z3roco01/lifed/LifedClient.java
+++ src/client/java/z3roco01/lifed/LifedClient.java
@@ -1,10 +1,14 @@
 package z3roco01.lifed;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LifedClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Lifed.MOD_ID + "-client");
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        LOGGER.info("this is your 3rd life");
 	}
 }
