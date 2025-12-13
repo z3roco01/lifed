88b4488648f5dfd513b244dc3608c6aad752818f
z3roco01 <zerocool@e.email>
2025-12-13 16:49:46 -0600

    add template data

---
+++ src/client/java/z3roco01/lifed/LifedClient.java
@@ -0,0 +1,10 @@
+package z3roco01.lifed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LifedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
