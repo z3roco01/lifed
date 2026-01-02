5ea9e2424c84c09113457fac0d3a4330a05e96d1
z3roco01 <zerocool@e.email>
2026-01-02 15:27:49 -0600

    allow totems to be turned into another life

---
+++ src/main/java/z3roco01/lifed/mixin/ItemMixin.java
@@ -0,0 +1,37 @@
+package z3roco01.lifed.mixin;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DeathProtectionComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.resource.featuretoggle.ToggleableFeature;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import z3roco01.lifed.features.LifeManager;
+import z3roco01.lifed.util.player.PlayerUtil;
+
+@Mixin(Item.class)
+public abstract class ItemMixin implements ToggleableFeature, ItemConvertible, FabricItem {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    private void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if(world.isClient()) return;
+
+        ItemStack stack = user.getStackInHand(hand);
+        DeathProtectionComponent deathProtection = stack.get(DataComponentTypes.DEATH_PROTECTION);
+
+        if(deathProtection != null) {
+            stack.decrement(1);
+            LifeManager.addLife((ServerPlayerEntity)user);
+            PlayerUtil.playTotemAnimation((ServerPlayerEntity)user);
+        }
+    }
+}
